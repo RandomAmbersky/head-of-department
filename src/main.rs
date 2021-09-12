a8ddf72da649d65e18abf8a81c566f0805a2c941
polyanin <pa@green-g.ru>
2021-09-12 20:05:48 +0300

    it worked

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 use ggez::{conf, event, Context, GameResult};
 use std::path;
 
-struct Game {}
+struct MyGame {}
 
-impl event::EventHandler<ggez::GameError> for Game {
+impl MyGame {
+    pub fn new(_ctx: &mut Context) -> MyGame {
+        // Load/create resources such as images here.
+        MyGame {
+            // ...
+        }
+    }
+}
+
+impl event::EventHandler<ggez::GameError> for MyGame {
     fn update(&mut self, _context: &mut Context) -> GameResult {
         // TODO: update game logic here
         Ok(())
@@ -19,26 +28,11 @@ pub fn main() -> GameResult {
 
     let cb = ggez::ContextBuilder::new("rust_sokoban", "sokoban")
         .window_setup(conf::WindowSetup::default().title("Rust Sokoban!"))
-        .window_mode(conf::WindowMode::default().dimensions(800.0, 600.0))
+        .window_mode(conf::WindowMode::default().dimensions(3000.0, 2000.0))
         .add_resource_path(path::PathBuf::from("./resources"));
 
-    let (context, event_loop) = cb.build()?;
-    let game = &mut Game {};
+    let (mut context, event_loop) = cb.build()?;
+    let game = MyGame::new(&mut context);
     event::run(context, event_loop, game)
 
-    // let (ctx, event_loop) = cb.build()?;
-    // let state = MainState::new()?;
-    // event::run(ctx, event_loop, state)
-
-    // // Create a game context and event loop
-    // let context_builder = ggez::ContextBuilder::new("rust_sokoban", "sokoban")
-    //     .window_setup(conf::WindowSetup::default().title("Rust Sokoban!"))
-    //     .window_mode(conf::WindowMode::default().dimensions(800.0, 600.0))
-    //     .add_resource_path(path::PathBuf::from("./resources"));
-    //
-    // let (context, event_loop) = &mut context_builder.build()?;
-    // // Create the game state
-    // let game = &mut Game {};
-    // // Run the main event loop
-    // event::run(context, event_loop, game)
 }
