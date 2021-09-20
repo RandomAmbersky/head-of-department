0c4fc658a26364d8afd02e174058fa326ee52826
polyanin <pa@green-g.ru>
2021-09-20 12:00:58 +0300

    it compiled

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,47 @@
-use ggez::{conf, event, Context, GameResult};
+use ggez::{conf, event, Context, GameResult, graphics};
 use std::path;
 use specs::{
     join::Join, Builder, Component, ReadStorage, RunNow, System, VecStorage, World, WorldExt,
 };
+use ggez::graphics::{Image, DrawParam};
+use ggez::nalgebra as na;
+
+const TILE_WIDTH: f32 = 32.0;
 
 pub struct RenderingSystem<'a> {
     context: &'a mut Context,
+}
+
+// System implementation
+impl<'a> System<'a> for RenderingSystem<'a> {
+    // Data
+    type SystemData = (ReadStorage<'a, Position>, ReadStorage<'a, Renderable>);
+
+    fn run(&mut self, data: Self::SystemData) {
+        let (positions, renderables) = data;
+
+        // Clearing the screen (this gives us the background colour)
+        graphics::clear(self.context, graphics::Color::new(0.95, 0.95, 0.95, 1.0));
+
+        // Get all the renderables with their positions and sort by the position z
+        // This will allow us to have entities layered visually.
+        let mut rendering_data = (&positions, &renderables).join().collect::<Vec<_>>();
+        rendering_data.sort_by_key(|&k| k.0.z);
+
+        // Iterate through all pairs of positions & renderables, load the image
+        // and draw it at the specified position.
+        for (position, renderable) in rendering_data.iter() {
+            // Load the image
+            let image = Image::new(self.context, renderable.path.clone()).expect("expected image");
+            let x = position.x as f32 * TILE_WIDTH;
+            let y = position.y as f32 * TILE_WIDTH;
+
+            // draw
+            let draw_params = DrawParam::new().dest(na::Point2::new(x, y));
+            graphics::draw(self.context, &image, draw_params).expect("expected render");
+        }
+
+    }
 }
 
 // Components
@@ -104,36 +140,45 @@ pub fn create_player(world: &mut World, position: Position) {
         .build();
 }
 
-struct MyGame {}
+struct MyGame {
+    world: World
+}
 
 impl MyGame {
-    pub fn new(_ctx: &mut Context) -> MyGame {
+    pub fn new(_ctx: &mut Context, world: World) -> MyGame {
         // Load/create resources such as images here.
         MyGame {
-            // ...
+            world
         }
     }
 }
 
-impl event::EventHandler<ggez::GameError> for MyGame {
+impl event::EventHandler for MyGame {
     fn update(&mut self, _context: &mut Context) -> GameResult {
         // TODO: update game logic here
         Ok(())
     }
 
-    fn draw(&mut self, _context: &mut Context) -> GameResult {
-        // TODO: update draw here
+    fn draw(&mut self, context: &mut Context) -> GameResult {
+        // Render game entities
+        {
+            let mut rs = RenderingSystem { context };
+            rs.run_now(&self.world);
+        }
         Ok(())
     }
 }
 
 pub fn main() -> GameResult {
+    let mut world = World::new();
+    register_components(&mut world);
+
     let cb = ggez::ContextBuilder::new("rust_sokoban", "sokoban")
         .window_setup(conf::WindowSetup::default().title("Rust Sokoban!"))
-        .window_mode(conf::WindowMode::default().dimensions(3000.0, 2000.0))
+        .window_mode(conf::WindowMode::default().dimensions(1024.0, 768.0))
         .add_resource_path(path::PathBuf::from("./resources"));
 
-    let (mut context, event_loop) = cb.build()?;
-    let game = MyGame::new(&mut context);
-    event::run(context, event_loop, game)
+    let (context, event_loop) = &mut cb.build()?;
+    let mut game = MyGame::new(context, world);
+    event::run(context, event_loop,  &mut game)
 }
