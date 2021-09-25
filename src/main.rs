1799a03143f43e5fbe1c9edda8fc7bbe09ba5cbe
sir Random <RandomAmbersky@gmail.com>
2021-09-25 15:28:09 +0300

    some refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,25 @@
+mod processing;
+
 #[macro_use]
 extern crate glium;
 
-use glium::glutin::window::WindowId;
-use glium::glutin::event::Event;
-use glium::glutin::event::WindowEvent;
-use glium::glutin::event_loop::ControlFlow;
-use glium::glutin;
-
-fn process_window_event (window_id: WindowId, event: WindowEvent, control_flow: &mut ControlFlow) {
-	match event {
-		WindowEvent::Resized(_) => {}
-		WindowEvent::Moved(_) => {}
-		WindowEvent::CloseRequested => {
-			*control_flow = glutin::event_loop::ControlFlow::Exit;
-			return;
-		}
-		WindowEvent::Destroyed => {}
-		WindowEvent::DroppedFile(_) => {}
-		WindowEvent::HoveredFile(_) => {}
-		WindowEvent::HoveredFileCancelled => {}
-		WindowEvent::ReceivedCharacter(_) => {}
-		WindowEvent::Focused(_) => {}
-		WindowEvent::KeyboardInput { .. } => {}
-		WindowEvent::ModifiersChanged(_) => {}
-		WindowEvent::CursorMoved { .. } => {}
-		WindowEvent::CursorEntered { .. } => {}
-		WindowEvent::CursorLeft { .. } => {}
-		WindowEvent::MouseWheel { .. } => {}
-		WindowEvent::MouseInput { .. } => {}
-		WindowEvent::TouchpadPressure { .. } => {}
-		WindowEvent::AxisMotion { .. } => {}
-		WindowEvent::Touch(_) => {}
-		WindowEvent::ScaleFactorChanged { .. } => {}
-		WindowEvent::ThemeChanged(_) => {}
-	}
-}
-
-fn process_event(ev: Event<()>, control_flow: &mut ControlFlow) {
-	match ev {
-		Event::NewEvents(_) => {},
-		Event::WindowEvent { window_id, event} => {
-			process_window_event(window_id, event, control_flow);
-			return;
-		},
-		Event::DeviceEvent { .. } => {},
-		Event::UserEvent(_) => {},
-		Event::Suspended => {},
-		Event::Resumed => {},
-		Event::MainEventsCleared => {},
-		Event::RedrawRequested(_) => {},
-		Event::RedrawEventsCleared => {},
-		Event::LoopDestroyed => {}
-	}
-}
+use glium::{glutin, Surface};
 
 fn main() {
-	use glium::glutin;
-
-	let mut event_loop = glutin::event_loop::EventLoop::new();
+	let event_loop = glutin::event_loop::EventLoop::new();
 	let wb = glutin::window::WindowBuilder::new();
 	let cb = glutin::ContextBuilder::new();
 	let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
 	event_loop.run( move |event, _target, control_flow| {
+
+		let mut target = display.draw();
+		target.clear_color(0.0, 0.0, 1.0, 1.0);
+		target.finish().unwrap();
+
 		let next_frame_time = std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
 		glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
-		process_event(event, control_flow);
+
+		processing::process_event(event, control_flow);
 	});
 }
---
+++ src/processing.rs
@@ -0,0 +1,51 @@
+	use glium::glutin::window::WindowId;
+	use glium::glutin::event::Event;
+	use glium::glutin::event::WindowEvent;
+	use glium::glutin::event_loop::ControlFlow;
+
+	fn process_window_event (_window_id: WindowId, event: WindowEvent, control_flow: &mut ControlFlow) {
+		match event {
+			WindowEvent::Resized(_) => {}
+			WindowEvent::Moved(_) => {}
+			WindowEvent::CloseRequested => {
+				*control_flow = glium::glutin::event_loop::ControlFlow::Exit;
+				return;
+			}
+			WindowEvent::Destroyed => {}
+			WindowEvent::DroppedFile(_) => {}
+			WindowEvent::HoveredFile(_) => {}
+			WindowEvent::HoveredFileCancelled => {}
+			WindowEvent::ReceivedCharacter(_) => {}
+			WindowEvent::Focused(_) => {}
+			WindowEvent::KeyboardInput { .. } => {}
+			WindowEvent::ModifiersChanged(_) => {}
+			WindowEvent::CursorMoved { .. } => {}
+			WindowEvent::CursorEntered { .. } => {}
+			WindowEvent::CursorLeft { .. } => {}
+			WindowEvent::MouseWheel { .. } => {}
+			WindowEvent::MouseInput { .. } => {}
+			WindowEvent::TouchpadPressure { .. } => {}
+			WindowEvent::AxisMotion { .. } => {}
+			WindowEvent::Touch(_) => {}
+			WindowEvent::ScaleFactorChanged { .. } => {}
+			WindowEvent::ThemeChanged(_) => {}
+		}
+	}
+
+	pub(crate) fn process_event(ev: Event<()>, control_flow: &mut ControlFlow) {
+		match ev {
+			Event::NewEvents(_) => {},
+			Event::WindowEvent { window_id, event } => {
+				process_window_event(window_id, event, control_flow);
+				return;
+			},
+			Event::DeviceEvent { .. } => {},
+			Event::UserEvent(_) => {},
+			Event::Suspended => {},
+			Event::Resumed => {},
+			Event::MainEventsCleared => {},
+			Event::RedrawRequested(_) => {},
+			Event::RedrawEventsCleared => {},
+			Event::LoopDestroyed => {}
+		}
+	}
